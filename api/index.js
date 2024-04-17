6126a2c1b264a56831064754e68b2dc97cfb01f4
Ivan Chen <64084815+Decadence69@users.noreply.github.com>
2024-04-18 01:04:58 +0800

    mongoose models test deploy

--- api/index.js
+++ api/index.js
@@ -35,12 +35,12 @@ connectDB();
 // })
 // .catch((e) => console.log(e));
 
-// require("./userLoginDetails");
-// require("./debateDetails");
-// require("./debateRoom");
+require("./userLoginDetails");
+require("./debateDetails");
+require("./debateRoom");
 
-// const User = mongoose.model("users");
-// const Debate = mongoose.model("debates");
+const User = mongoose.model("users");
+const Debate = mongoose.model("debates");
 // // const DebateRoom = mongoose.model("debateRooms");
 
 app.post("/signup", async (req, res) => {
