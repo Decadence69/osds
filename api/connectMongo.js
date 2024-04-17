085078ee10da11d2e72c5101e0036534e94efb43
Ivan Chen <64084815+Decadence69@users.noreply.github.com>
2024-04-18 01:28:25 +0800

    dep test

--- api/connectMongo.js
+++ api/connectMongo.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("Connected to MongoDB successfully");
+        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+        console.log("Connect to MongoDB successfully")
     } catch (error) {
-        console.error("Failed to connect to MongoDB:", error.message);
+        console.log("Connect failed " + error.message )
     }
-};
+}
 
-module.exports = connectDB;
+module.exports = connectDB
